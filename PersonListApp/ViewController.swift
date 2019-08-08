c0044e3eded32a79a84e048c5d3a0c414451a109
John Heifetz <skidheif@gmail.com>
2019-08-08 09:14:38 +0300

    Initial Commit

---
+++ PersonListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonListApp
+//
+//  Created by John Heifetz on 08/08/2019.
+//  Copyright © 2019 heif_team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
